778974a4300b25e46fac815609ecef23de7f77ce
call me kipin <kipinnn873@gmail.com>
2025-06-18 09:19:07 +0700

    update: fixing some minor problems

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -19,9 +19,17 @@ const Breadcrumbs = () => {
             .replace(/-/g, " ")
             .replace(/\b\w/g, (char) => char.toLowerCase());
 
+          // Cek apakah path mengandung pola 'edit-*'
+          const editMatch = name.match(/^edit-(.+)/);
+
           return (
-            <li key={name} className="text-[#FF800A]">
-              {isLast ? (
+            <li key={index} className="text-[#FF800A]">
+              {editMatch ? (
+                // Jika cocok dengan edit-*, render <p> untuk bagian edit dan Link untuk bagian selanjutnya (jika ada)
+                <>
+                  <p>{`edit ${editMatch[1].replace(/-/g, " ")}`}</p>
+                </>
+              ) : isLast ? (
                 <span>{label}</span>
               ) : (
                 <Link to={routeTo}>{label}</Link>
